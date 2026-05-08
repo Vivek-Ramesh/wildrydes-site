8cb7959dabb3e1e423ad30eba8b0181b371f62ce
Vivek-Ramesh <59198660+Vivek-Ramesh@users.noreply.github.com>
2026-05-08 03:26:00 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_ZLcJ0jaAv',
         userPoolClientId: '3ohbd0lrmjgif81tt97cvprod5',
-        region: 'us-east-1' // e.g. us-east-2
+        region: 'us-east-1'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://g713zgl4sd.execute-api.us-east-1.amazonaws.com/dev'
     }
 };
